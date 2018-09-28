656e6131125368e62c1b223f67cbe03fc6e1f99c
Daichi Ando <d_ando@gizumo-admin-07noMacBook-puro.local>
2018-09-28 15:39:56 +0900

    first_commit

---
+++ app/Http/Controllers/ScrapeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ScrapeController extends Controller
+{
+    public function index()
+    {
+        exit('here');
+    }
+
+    public function show()
+    {
+        exit('show');
+    }
+}
