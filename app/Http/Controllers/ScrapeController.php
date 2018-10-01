db41bbb6321683057732cff196e66dd347790f03
Daichi Ando <d_ando@gizumo-admin-07noMacBook-puro.local>
2018-10-01 12:30:58 +0900

    scrape_logic_complete

--- app/Http/Controllers/ScrapeController.php
+++ app/Http/Controllers/ScrapeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Goutte;
+
+define('TARGET_URL', 'http://gizumo-inc.jp/company/members/');
 
 class ScrapeController extends Controller
 {
@@ -13,6 +16,27 @@ class ScrapeController extends Controller
 
     public function show()
     {
-        exit('show');
+        $member_infos = $this->scrape_page();
+        dd($infos);
+    }
+
+    public function scrape_page()
+    {
+        $crawler = Goutte::request('GET', TARGET_URL);
+        return $crawler->filter('.content__grid--4__item')->each(function ($node) {
+            return [
+                'name'    => $node->filter('.member-column__name')->text(),
+                'en_name' => $node->filter('.member-column__name--en')->text(),
+                'post'    => $node->filter('.member-column__post')->text(),
+                'intro'   => $node->filter('.member-column__bubble')->text(),
+                'img'     => $this->extract_url($node->filter('.member-column__image')->attr('style')),
+            ];
+        });
+    }
+
+    public function extract_url($style)
+    {
+        return mb_substr($style, 22, -2);
     }
 }
+
