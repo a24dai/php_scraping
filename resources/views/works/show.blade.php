3befd5adff9358938495da80207363f4111da2fc
Daichi Ando <d_ando@gizumo-admin-07noMacBook-puro.local>
2018-10-01 16:09:36 +0900

    delete_for_lesson

--- resources/views/works/show.blade.php
+++ resources/views/works/show.blade.php
@@ -1,14 +1,12 @@
 @extends('common.common')
 @section('content')
 
-    @foreach ($member_infos as $member)
       <ul>
-        <li><p>{{{ $member['name'] }}}</p></li>
-        <li><p>{{{ $member['en_name'] }}}</p></li>
-        <li><p>{{{ $member['post'] }}}</p></li>
-        <li><p>{{{ $member['intro'] }}}</p></li>
-        <li><img src="{{{ $member['img'] }}}"></li>
+        <li><p></p></li>
+        <li><p></p></li>
+        <li><p></p></li>
+        <li><p></p></li>
+        <li><img src=""></li>
       </ul>
-    @endforeach
 
 @endsection
